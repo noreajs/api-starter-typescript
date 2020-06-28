5c41e63831b7e41fbd16e1e5ca8364ec62289475
Arnold L <34209333+lambou@users.noreply.github.com>
2020-06-28 13:59:40 +0100

    initial commit

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,30 +1,24 @@
-import { Application } from "express"
-import authController from "../controllers/auth.controller"
-import { Oauth } from "@noreajs/oauth-v2-provider-me"
-import { NoreaApplication } from "@noreajs/core"
+import authController from "../controllers/auth.controller";
+import { Oauth } from "@noreajs/oauth-v2-provider-me";
+import { NoreaApplication } from "@noreajs/core";
 
 export default (app: NoreaApplication) => {
-    /**
-     * Register
-     */
-    app.route('/register').post([
-        authController.register
-    ])
+  /**
+   * Register
+   */
+  app.route("/register").post([authController.register]);
 
-    /**
-     * Update account locale
-     */
-    app.route('/account/update/locale').put([
-        Oauth.authorize(),
-        authController.updateLocale
-    ])
+  /**
+   * Update account locale
+   */
+  app
+    .route("/account/update/locale")
+    .put([Oauth.authorize(), authController.updateLocale]);
 
-    /**
-     * Update account
-     */
-    app.route('/account/update').put([
-        Oauth.authorize(),
-        authController.updateAccount
-    ])
-
-}
+  /**
+   * Update account
+   */
+  app
+    .route("/account/update")
+    .put([Oauth.authorize(), authController.updateAccount]);
+};
